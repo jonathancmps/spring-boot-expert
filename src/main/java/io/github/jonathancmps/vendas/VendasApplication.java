f72993ccbb5a01c871bf3fca4256161c7c67485c
Jonathan Campos <jonathan.campos.net@gmail.com>
2024-03-02 23:48:23 -0300

    resetando para configuracoes iniciais

--- src/main/java/io/github/jonathancmps/vendas/VendasApplication.java
+++ src/main/java/io/github/jonathancmps/vendas/VendasApplication.java
@@ -9,16 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class VendasApplication {
-
-	@Value("${application.name}")
-	private String applicationName;
-
-	@GetMapping("/hello")
-	public String helloWord(){
-		return applicationName;
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
 	}
